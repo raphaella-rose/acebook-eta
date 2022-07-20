20ab02d9b19d5109f31de0c5eb20bbd3e4b93f33
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-07-20 12:46:18 +0100

    clean up lines

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,22 +1,18 @@
-let likes = document.querySelector(".likes");
-let likeButton = document.querySelector("likeButton");
-
-// let likeCount = 0;
-
-
-
 const likeFunction = (id) => {
-  const data = {id: id}
-  fetch("/posts/like", {
+  const data = { id: id };
+
+  const options = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
+  };
+
+  fetch("/posts/like", options)
     .then((response) => response.json())
     .then((data) => {
-      const span = document.querySelector(`#likes-${id}`)
+      const span = document.querySelector(`#likes-${id}`);
       span.innerHTML = data.likes;
     });
 };
